8801637f43c5d0340dc37ac547de4d53a7d7437d
zphrs <z@zephiris.dev>
2025-02-23 16:45:57 -0800

    added license

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css"
 import { mockDate } from "./mockdate.ts"
-import { createSandbox } from "./counter.ts"
+import { createSandbox } from "./sandbox.ts"
 
 let parent = document.querySelector<HTMLDivElement>("#app")!
 createSandbox(parent)
