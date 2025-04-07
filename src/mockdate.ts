d70bc903161969cc3b802968430c3dbf27725069
zphrs <z@zephiris.dev>
2025-04-07 10:05:40 -0700

    fixed date constructor loophole

--- src/mockdate.ts
+++ src/mockdate.ts
@@ -4,6 +4,46 @@ export function mockDate(d: string | number | Date) {
   let unmocked = globalThis.Date
   let date: Date = new unmocked(d)
   let startOfMock = unmocked.now()
+  const getDateProxy = () =>
+    new Proxy(unmocked, {
+      apply: function (Target, thisArg, args) {
+        return new Proxy(
+          new Target(date.getTime() + (unmocked.now() - startOfMock)),
+          {
+            get(target, symbol, receiver) {
+              if (symbol.toString() == "__proto__") {
+                throw new Error("Cannot get proto")
+              }
+              if (symbol.toString() == "constructor") {
+                throw new Error("Cannot get constructor")
+              }
+              // @ts-expect-error
+              return Reflect.get(...arguments)
+            },
+          }
+        )
+      },
+      construct: function (Target, args) {
+        // props to https://stackoverflow.com/a/43160428/671457
+        if (args.length === 0) {
+          return new Target(date.getTime() + (unmocked.now() - startOfMock))
+        }
+        // @ts-expect-error
+        return new Target(...args)
+      },
+      get: function (Target, prop, receiver) {
+        if (prop === "now") {
+          return () => date.getTime() * 1 + (unmocked.now() - startOfMock)
+        }
+        if (prop === "constructor") {
+          return (...props: any[]) => {
+            receiver(...props)
+          }
+        }
+        // @ts-expect-error
+        return Reflect.get(...arguments)
+      },
+    })
   globalThis.Date = new Proxy(unmocked, {
     apply: function (Target, thisArg, args) {
       return new Target(
@@ -21,6 +61,11 @@ export function mockDate(d: string | number | Date) {
     get: function (Target, prop, receiver) {
       if (prop === "now") {
         return () => date.getTime() * 1 + (unmocked.now() - startOfMock)
+      }
+      if (prop === "constructor") {
+        return (...props: any[]) => {
+          new receiver(props)
+        }
       }
       // @ts-expect-error
       return Reflect.get(...arguments)
