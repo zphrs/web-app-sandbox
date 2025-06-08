a0113626b2adedb4fe4cecb3401a38924045ab65
zphrs <z@zephiris.dev>
2025-06-07 22:39:21 -0700

    cleaned up main page's sw stud

--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,7 @@
 /// <reference lib="webworker" />
-const sw = self as unknown as ServiceWorkerGlobalScope
+// const sw = self as unknown as ServiceWorkerGlobalScope
 
-sw.addEventListener("install", event => {})
+// sw.addEventListener("install", event => {})
 
 // sw.addEventListener("fetch", event => {
 //   // console.log(event.request.url)
